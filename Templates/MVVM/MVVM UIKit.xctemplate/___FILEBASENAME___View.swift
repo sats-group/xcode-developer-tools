41fc2340b78d8f7a39730f1d45fb4dc64c140763
Kent Robin Haugen <kentrh@gmail.com>
2021-01-29 13:06:55 +0100

    Added preview for view, remove sample prop from view data

--- Templates/MVVM/MVVM UIKit.xctemplate/___FILEBASENAME___View.swift
+++ Templates/MVVM/MVVM UIKit.xctemplate/___FILEBASENAME___View.swift
@@ -31,3 +31,22 @@ extension ___VARIABLE_viewName___ {
 
     }
 }
+
+// MARK: - Preview
+
+import SwiftUI
+
+@available(iOS 14.0.0, *)
+struct ___VARIABLE_viewName____Preview: PreviewProvider {
+    static func getPreview() -> UIView {
+        let view = ___VARIABLE_viewName___(withAutoLayout: true)
+        let viewData = ___VARIABLE_viewDataName___()
+        view.configure(with: viewData)
+        return view
+    }
+    
+    static var previews: some View {
+        DemoWrapperView(view: getPreview())
+            .ignoresSafeArea()
+    }
+}
